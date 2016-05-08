49731a208230345b92832d8e92ee05039e9d60d3
Spiros <smouzakitis@epu.ntua.gr>
2016-05-09 00:00:35 +0300

    Created base template and first sample page for the dashboard

--- derisk_app/views.py
+++ derisk_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+@login_required()
 def overview(request):
-    return render(request, 'overview.html')
+    params = {}
+    params['username'] = request.user.username
+    return render(request, 'overview.html',params)
