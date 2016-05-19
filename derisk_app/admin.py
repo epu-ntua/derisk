24e2ad6fa7f01567f7fb196000b06c6f5963b4ed
Spiros <smouzakitis@epu.ntua.gr>
2016-05-19 18:35:49 +0300

    First schema attempt and fixture data for Measures

--- derisk_app/admin.py
+++ derisk_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from derisk_app.models import *
 # Register your models here.
+
+@admin.register(Measure)
+class MeasureAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
---
+++ derisk_app/lists.py
@@ -0,0 +1,71 @@
+﻿COUNTRIES = (
+    ('', '----------'),
+    ('AT', 'Austria'),
+    ('BE', 'Belgium'),
+    ('BG', 'Bulgaria'),
+    ('CA', 'Canada'),
+    ('CY', 'Cyprus'),
+    ('CZ', 'Czech Republic'),
+    ('DE', 'Germany'),
+    ('DK', 'Denmark'),
+    ('EE', 'Estonia'),
+    ('EL', 'Greece'),
+    ('ES', 'Spain'),
+    ('FI', 'Finland'),
+    ('FR', 'France'),
+    ('HR', 'Croatia'),
+    ('HU', 'Hungary'),
+    ('IE', 'Ireland'),
+    ('IT', 'Italy'),
+    ('LT', 'Lithuania'),
+    ('LU', 'Luxembourg'),
+    ('LV', 'Latvia'),
+    ('MT', 'Malta'),
+    ('NL', 'Netherlands'),
+    ('PL', 'Poland'),
+    ('PT', 'Portugal'),
+    ('RO', 'Romania'),
+    ('SE', 'Sweden'),
+    ('SI', 'Slovenia'),
+    ('SK', 'Slovak Republic'),
+    ('UK', 'United Kingdom'),
+    ('US', 'United States'),
+)
+
+ORGANIZATIONSIZE = (
+    ('','----'),
+    ('MICRO','micro enterprises: with less than 10 persons employed'),
+    ('SMALL','small enterprises: with 10-49 persons employed'),
+    ('MEDIUM','medium-sized enterprises: with 50-249 persons employed'),
+    ('LARGE','large enterprises: with 250 or more persons employed'),
+)
+
+BUILDINGTYPE = (
+   ('','----'),
+   ('DETACHED','detached single family dwellings'),
+   ('SINGLE','other single family dwellings'),
+   ('MULTI4','multi-family buildings 1-4 storeys'),
+   ('MULTI5','multi-family buidings 5+ storeys'),
+   ('PRIVATE','private offices'),
+   ('PUBLIC','public buildings'),
+   ('WHOLESALE','wholesale and retail trade'),
+   ('HOTEL','hotel & restaurants'),
+   ('HEALTH','health care'),
+   ('EDUCATION','educational buildings'),
+   ('SPORT','sport facilities'),
+   ('INDUSTRY','industry'),
+)
+
+OWNERSHIP = (
+   ('','----'),
+   ('OWN','owned'),
+   ('RENT','rented'),
+)
+
+VERIFIED = (
+  ('','----'),
+  ('By third party','By third party'),
+  ('by ESCO','by ESCO'),
+  ('by other','by other'),
+  ('not verified','not verified'),
+)
